5b094e0d3c1eeb8802a298cb906c47167ae8a13d
Yanyuan Yu <iphoneiid@163.com>
2026-06-18 17:15:09 +1200

    Update 05_churn_by_ payment_method.sql

--- queries/05_churn_by_ payment_method.sql
+++ queries/05_churn_by_ payment_method.sql
@@ -1,1 +1,14 @@
+-- 05
 
+-- Objective: To analyze payment methods and churn.
+SELECT
+    PaymentMethod,
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        100.0 * SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) / COUNT(*),
+        2
+    ) AS churn_rate_pct
+FROM telco_customers
+GROUP BY PaymentMethod
+ORDER BY churn_rate_pct DESC;
